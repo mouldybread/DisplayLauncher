d8a5e03857df8e6e56de6f7f47f9ce64ba733a48
mouldybread <38598858+mouldybread@users.noreply.github.com>
2025-10-24 05:12:59 +0800

    Update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.tpn.displaylauncher"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 4
+        versionName = "0.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
