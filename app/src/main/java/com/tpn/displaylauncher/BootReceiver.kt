7f8264d105270caa4ec74eb6ff7badd00503849c
mouldybread <38598858+mouldybread@users.noreply.github.com>
2026-08-23 00:43:20 +0800

    Bump gradle, implement BroadcastReceiver using ACTION_BOOT_COMPLETED

---
+++ app/src/main/java/com/tpn/displaylauncher/BootReceiver.kt
@@ -0,0 +1,16 @@
+package com.tpn.displaylauncher
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import androidx.core.content.ContextCompat
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED ||
+            intent.action == Intent.ACTION_LOCKED_BOOT_COMPLETED) {
+            val serviceIntent = Intent(context, LauncherService::class.java)
+            ContextCompat.startForegroundService(context, serviceIntent)
+        }
+    }
+}
